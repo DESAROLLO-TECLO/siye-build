08532be9da2c7dfd853cbb516910beeee0b53dd5
blazerlee <isc.danivalencia@gmail.com>
2020-03-24 20:11:13 -0600

    servicios  para catalogos

--- siye-api/src/main/java/mx/com/teclo/siye/negocio/service/catalogo/CatalogoService.java
+++ siye-api/src/main/java/mx/com/teclo/siye/negocio/service/catalogo/CatalogoService.java
@@ -4,9 +4,11 @@ import java.util.List;
 
 import mx.com.teclo.arquitectura.ortogonales.exception.NotFoundException;
 import mx.com.teclo.siye.persistencia.vo.catalogo.StEncuestaVO;
+import mx.com.teclo.siye.persistencia.vo.catalogo.TipoVehiculoVO;
 
 public interface CatalogoService {
 	
 	public List<StEncuestaVO> estatusEncuesta() throws NotFoundException;
-
+	
+	public List<TipoVehiculoVO> tipoVehiculo() throws NotFoundException;
 }
--- siye-api/src/main/java/mx/com/teclo/siye/negocio/service/catalogo/CatalogoServiceImpl.java
+++ siye-api/src/main/java/mx/com/teclo/siye/negocio/service/catalogo/CatalogoServiceImpl.java
@@ -5,19 +5,27 @@ import java.util.List;
 
 import javax.transaction.Transactional;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import mx.com.teclo.arquitectura.ortogonales.exception.NotFoundException;
 import mx.com.teclo.arquitectura.ortogonales.util.ResponseConverter;
 import mx.com.teclo.siye.persistencia.hibernate.dao.catalogo.StEncuestaDAO;
+import mx.com.teclo.siye.persistencia.hibernate.dao.catalogo.TipoVehiculoDAO;
 import mx.com.teclo.siye.persistencia.hibernate.dto.catalogo.StEncuestaDTO;
+import mx.com.teclo.siye.persistencia.hibernate.dto.proceso.TipoVehiculoDTO;
 import mx.com.teclo.siye.persistencia.vo.catalogo.StEncuestaVO;
+import mx.com.teclo.siye.persistencia.vo.catalogo.TipoVehiculoVO;
 import mx.com.teclo.siye.util.enumerados.RespuestaHttp;
 
 @Service
 public class CatalogoServiceImpl implements CatalogoService{
 	
+	@Autowired
 	private StEncuestaDAO stEncuestaDAO;
+	
+	@Autowired
+	private TipoVehiculoDAO tipoVehiculoDAO; 
 
 	@Transactional
 	@Override
@@ -27,6 +35,16 @@ public class CatalogoServiceImpl implements CatalogoService{
 			throw new NotFoundException(RespuestaHttp.NOT_FOUND.getMessage());
 		List<StEncuestaVO> listStEncuesta = ResponseConverter.converterLista(new ArrayList<>(), stEncuesta, StEncuestaVO.class);
 		return listStEncuesta;
+	}
+
+	@Transactional
+	@Override
+	public List<TipoVehiculoVO> tipoVehiculo() throws NotFoundException {
+		List<TipoVehiculoDTO> tpVehiculo = tipoVehiculoDAO.tipoVehiculo();
+		if(tpVehiculo.isEmpty())
+			throw new NotFoundException(RespuestaHttp.NOT_FOUND.getMessage());
+		List<TipoVehiculoVO> listTpVehiculo = ResponseConverter.converterLista(new ArrayList<>(), tpVehiculo, TipoVehiculoVO.class);
+		return listTpVehiculo;
 	}
 
 }
