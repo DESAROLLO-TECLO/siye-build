25837fcc35105d449a03990f16fd9ec30a9c039b
davidGuerra2794 <dguerradelamora@gmail.com>
2020-03-25 10:31:27 -0600

    Resolviendo conflicto

--- siye-api/src/main/java/mx/com/teclo/siye/persistencia/hibernate/dao/proceso/OrdenServicioDAO.java
+++ siye-api/src/main/java/mx/com/teclo/siye/persistencia/hibernate/dao/proceso/OrdenServicioDAO.java
@@ -1,22 +1,5 @@
 package mx.com.teclo.siye.persistencia.hibernate.dao.proceso;
 
-import mx.com.teclo.arquitectura.persistencia.comun.dao.BaseDao;
-import mx.com.teclo.siye.persistencia.hibernate.dto.proceso.OrdenServicioDTO;
-
-public interface OrdenServicioDAO extends BaseDao<OrdenServicioDTO>{
-
-	/**
-     * Descripción: Obtener el registro de orden de 
-     * servicio mediante su identificador unico
-     * @author Jorge Luis
-     * @param idOrdenServicio
-     * @return OrdenServicioDTO
-     * */
-	public OrdenServicioDTO obtenerOrdenServicio(Long idOrdenServicio);
-	
-}
-package mx.com.teclo.siye.persistencia.hibernate.dao.proceso;
-
 import java.util.List;
 
 import mx.com.teclo.arquitectura.persistencia.comun.dao.BaseDao;
--- siye-api/src/main/java/mx/com/teclo/siye/persistencia/hibernate/dao/proceso/OrdenServicioDAOImpl.java
+++ siye-api/src/main/java/mx/com/teclo/siye/persistencia/hibernate/dao/proceso/OrdenServicioDAOImpl.java
@@ -1,26 +1,5 @@
 package mx.com.teclo.siye.persistencia.hibernate.dao.proceso;
 
-import org.hibernate.Criteria;
-import org.hibernate.criterion.Restrictions;
-import org.springframework.stereotype.Repository;
-
-import mx.com.teclo.arquitectura.persistencia.comun.dao.BaseDaoHibernate;
-import mx.com.teclo.siye.persistencia.hibernate.dto.proceso.OrdenServicioDTO;
-
-@Repository
-public class OrdenServicioDAOImpl extends BaseDaoHibernate<OrdenServicioDTO> implements OrdenServicioDAO{
-
-	@Override
-	public OrdenServicioDTO obtenerOrdenServicio(Long idOrdenServicio) {
-		Criteria c = getCurrentSession().createCriteria(OrdenServicioDTO.class);
-		c.add(Restrictions.eq("stActivo", true));
-		c.add(Restrictions.eq("idOrdenServicio", idOrdenServicio));
-		return (OrdenServicioDTO) c.uniqueResult();
-	}
-
-}
-package mx.com.teclo.siye.persistencia.hibernate.dao.proceso;
-
 import java.util.List;
 
 import org.hibernate.Criteria;
