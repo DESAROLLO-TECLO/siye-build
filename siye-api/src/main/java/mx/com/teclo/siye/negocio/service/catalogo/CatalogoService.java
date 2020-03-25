3591634894a27b0edab475ac4952153ad1759d86
JoseCastillo48 <josec.castillo48@gmail.com>
2020-03-25 12:41:28 -0600

    Se agrega consulta de conductores e instaladores CatalogoService.java

--- siye-api/src/main/java/mx/com/teclo/siye/negocio/service/catalogo/CatalogoService.java
+++ siye-api/src/main/java/mx/com/teclo/siye/negocio/service/catalogo/CatalogoService.java
@@ -3,6 +3,8 @@ package mx.com.teclo.siye.negocio.service.catalogo;
 import java.util.List;
 
 import mx.com.teclo.arquitectura.ortogonales.exception.NotFoundException;
+import mx.com.teclo.siye.persistencia.vo.catalogo.ConductorVO;
+import mx.com.teclo.siye.persistencia.vo.catalogo.InstaladorVO;
 import mx.com.teclo.siye.persistencia.vo.catalogo.StEncuestaVO;
 import mx.com.teclo.siye.persistencia.vo.catalogo.TipoVehiculoVO;
 
@@ -11,4 +13,8 @@ public interface CatalogoService {
 	public List<StEncuestaVO> estatusEncuesta() throws NotFoundException;
 	
 	public List<TipoVehiculoVO> tipoVehiculo() throws NotFoundException;
+	
+	public List<ConductorVO> getTransportistas() throws NotFoundException;
+	
+	public List<InstaladorVO> getTecnicos() throws NotFoundException;
 }
