3d54357523e46e5760592a481b8c9129a964c11c
Maverick <isc.foctaviosanchez@gmail.com>
2020-03-25 14:27:22 -0600

    dao de encuentas

---
+++ siye-api/src/main/java/mx/com/teclo/siye/persistencia/hibernate/dao/encuesta/EncuestasDAO.java
@@ -0,0 +1,8 @@
+package mx.com.teclo.siye.persistencia.hibernate.dao.encuesta;
+
+import mx.com.teclo.arquitectura.persistencia.comun.dao.BaseDao;
+import mx.com.teclo.siye.persistencia.hibernate.dto.encuesta.EncuestasDTO;
+
+public interface EncuestasDAO extends BaseDao<EncuestasDTO> {
+
+}
