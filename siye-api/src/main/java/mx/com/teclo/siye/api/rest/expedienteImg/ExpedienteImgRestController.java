7e04c0cfb5683bf179409de4adfb30aa25463a53
Maverick <isc.foctaviosanchez@gmail.com>
2020-03-24 19:36:30 -0600

    dao's

---
+++ siye-api/src/main/java/mx/com/teclo/siye/api/rest/expedienteImg/ExpedienteImgRestController.java
@@ -0,0 +1,8 @@
+package mx.com.teclo.siye.api.rest.expedienteImg;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ExpedienteImgRestController {
+
+}
---
+++ siye-api/src/main/java/mx/com/teclo/siye/persistencia/hibernate/dao/procesos/IEProcesosDAO.java
@@ -0,0 +1,8 @@
+package mx.com.teclo.siye.persistencia.hibernate.dao.procesos;
+
+import mx.com.teclo.arquitectura.persistencia.comun.dao.BaseDao;
+import mx.com.teclo.siye.persistencia.hibernate.dto.procesos.IEprocesosDTO;
+
+public interface IEProcesosDAO extends BaseDao<IEprocesosDTO>{
+
+}
