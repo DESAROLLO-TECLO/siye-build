853d83ee59b1f7fdb8fcb33f5782e5d14c71da8f
ericrr21 <ericrr87@gmail.com>
2020-03-30 19:29:28 -0600

    validacion servicio de incidencia

--- siye-api/src/main/java/mx/com/teclo/siye/negocio/service/incidencia/IncidenciaServiceImpl.java
+++ siye-api/src/main/java/mx/com/teclo/siye/negocio/service/incidencia/IncidenciaServiceImpl.java
@@ -5,6 +5,7 @@ import javax.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import mx.com.teclo.arquitectura.ortogonales.exception.BusinessException;
 import mx.com.teclo.arquitectura.ortogonales.exception.NotFoundException;
 import mx.com.teclo.arquitectura.ortogonales.util.ResponseConverter;
 import mx.com.teclo.siye.persistencia.hibernate.dao.incidencia.IncidenciaDAO;
@@ -16,6 +17,9 @@ public class IncidenciaServiceImpl implements IncidenciaService {
 	
 	@Autowired
 	private IncidenciaDAO incidenciaDAO;
+	
+	private static final String MSG_ERROR_INCIDENCIA_NULA = "No se Encontraron Incidencias";
+
 
 	@Override
 	@Transactional
@@ -23,6 +27,11 @@ public class IncidenciaServiceImpl implements IncidenciaService {
 		IncidenciaVO iVO = new IncidenciaVO();
 		IncidenciaDTO iDTO = incidenciaDAO.getIncidenciabycdIncidencia(cdIncidencia);
 		iVO = ResponseConverter.copiarPropiedadesFull(iDTO, IncidenciaVO.class);
+		
+		if (iVO == null) {
+			throw new NotFoundException(MSG_ERROR_INCIDENCIA_NULA);
+		}
+		
 		return iVO;
 	}
 
