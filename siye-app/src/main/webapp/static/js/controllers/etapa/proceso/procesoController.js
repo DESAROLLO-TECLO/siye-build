dbcc7f530e502b3ccceae2cab02da99980542a7c
Jesus G Gomez <antonio.grande@unitis.com.mx>
2020-03-31 10:19:07 -0600

    se agregan cambios para continuar con el proceso de navegación para contestar una encuesta

--- siye-app/src/main/webapp/static/js/controllers/etapa/proceso/procesoController.js
+++ siye-app/src/main/webapp/static/js/controllers/etapa/proceso/procesoController.js
@@ -3,7 +3,8 @@ angular.module(appTeclo)
 function($rootScope,$scope,$window,$translate,$timeout,ModalService,procesoService,procesoInfo) {
 
     console.log(procesoInfo);
+    $scope.nombreEtapa = procesoInfo.data[0].idProceso.nbProceso;
+    $scope.dataEtapa = procesoInfo.data;
     $scope.stActivarEncuesta = false;
-    
     
 });
