0cf28b864099e1f8b20556cfc5cbff92feac4296
Jesus G Gomez <antonio.grande@unitis.com.mx>
2020-04-01 13:05:49 -0600

    proceso de encuesta con validación de cambios para mostrar activo o inactivo

--- siye-app/src/main/webapp/static/js/controllers/etapa/proceso/encuesta/encuestaController.js
+++ siye-app/src/main/webapp/static/js/controllers/etapa/proceso/encuesta/encuestaController.js
@@ -1,13 +1,6 @@
 angular.module(appTeclo)
 .controller("encuestaController",
 function($rootScope,$scope,$window,$translate,$timeout,ModalService,encuestaInfo) {
-
-    console.log("::::::::::::::::::::::::::::::::::");
-    console.log(encuestaInfo.data.encuesta.nbEncuesta);
-    console.log("::::::::::::::::::::::::::::::::::");
-    console.log(encuestaInfo.data.encuesta.secciones);
-    console.log("::::::::::::::::::::::::::::::::::");
-    
     $scope.nombEncuesta = encuestaInfo.data.encuesta.nbEncuesta;
     $scope.nombSeccion = encuestaInfo.data.encuesta.secciones[0].nbSeccion;
     $scope.seccEncuesta = encuestaInfo.data.encuesta.secciones[0];
