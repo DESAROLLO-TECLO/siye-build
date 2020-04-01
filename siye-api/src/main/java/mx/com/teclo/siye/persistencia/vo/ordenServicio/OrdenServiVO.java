18e8e3794a9dcbd97dcaf251ba2a49a917206b04
blazerlee <isc.danivalencia@gmail.com>
2020-03-31 21:29:36 -0600

    correcion nombre VO jdvp

--- siye-api/src/main/java/mx/com/teclo/siye/persistencia/vo/ordenServicio/OrdenServiVO.java
+++ siye-api/src/main/java/mx/com/teclo/siye/persistencia/vo/ordenServicio/OrdenServiVO.java
@@ -3,7 +3,8 @@ package mx.com.teclo.siye.persistencia.vo.ordenServicio;
 import java.util.List;
 
 import mx.com.teclo.siye.persistencia.vo.proceso.KitInstDispVO;
-import mx.com.teclo.siye.persistencia.vo.proceso.VehiculoVO;
+import mx.com.teclo.siye.persistencia.vo.proceso.VehiculoProcesoVO;
+
 
 public class OrdenServiVO {
 	
@@ -12,9 +13,17 @@ public class OrdenServiVO {
 	private Long plan;
 	private Long tpKit;
 	private String cdKitIntalacion;
-	private List<KitInstDispVO> kitInstDispVO;
-	private VehiculoVO vehiculoVO;
+	private List<KitInstDispVO> kitInstalacionVO;
+	private VehiculoProcesoVO vehiculoVO;
 	
+	
+	
+	public VehiculoProcesoVO getVehiculoVO() {
+		return vehiculoVO;
+	}
+	public void setVehiculoVO(VehiculoProcesoVO vehiculoVO) {
+		this.vehiculoVO = vehiculoVO;
+	}
 	public String getCdOrden() {
 		return cdOrden;
 	}
@@ -45,18 +54,14 @@ public class OrdenServiVO {
 	public void setCdKitIntalacion(String cdKitIntalacion) {
 		this.cdKitIntalacion = cdKitIntalacion;
 	}
-	public List<KitInstDispVO> getKitInstDispVO() {
-		return kitInstDispVO;
+	public List<KitInstDispVO> getKitInstalacionVO() {
+		return kitInstalacionVO;
 	}
-	public void setKitInstDispVO(List<KitInstDispVO> kitInstDispVO) {
-		this.kitInstDispVO = kitInstDispVO;
+	public void setKitInstalacionVO(List<KitInstDispVO> kitInstalacionVO) {
+		this.kitInstalacionVO = kitInstalacionVO;
 	}
-	public VehiculoVO getVehiculoVO() {
-		return vehiculoVO;
-	}
-	public void setVehiculoVO(VehiculoVO vehiculoVO) {
-		this.vehiculoVO = vehiculoVO;
-	}
+	
+	
 	
 	
 	
