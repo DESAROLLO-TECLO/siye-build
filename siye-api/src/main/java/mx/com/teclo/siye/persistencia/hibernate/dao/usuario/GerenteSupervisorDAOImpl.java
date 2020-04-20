31500ac1fd5600851e84387a0554f85922794600
JoseCastillo48 <josec.castillo48@gmail.com>
2020-04-20 14:18:43 -0500

    Modificacion DTOs CentroInstalacionDTO.java y GerenteSupervisorDTO.java, GerenteSupervisorDAOImpl.java

--- siye-api/src/main/java/mx/com/teclo/siye/persistencia/hibernate/dao/usuario/GerenteSupervisorDAOImpl.java
+++ siye-api/src/main/java/mx/com/teclo/siye/persistencia/hibernate/dao/usuario/GerenteSupervisorDAOImpl.java
@@ -3,6 +3,8 @@ package mx.com.teclo.siye.persistencia.hibernate.dao.usuario;
 import java.util.List;
 
 import org.hibernate.Criteria;
+import org.hibernate.criterion.Criterion;
+import org.hibernate.criterion.LogicalExpression;
 import org.hibernate.criterion.Projections;
 import org.hibernate.criterion.Restrictions;
 import org.springframework.stereotype.Repository;
@@ -27,9 +29,12 @@ public class GerenteSupervisorDAOImpl extends BaseDaoHibernate<GerenteSupervisor
 	public List<Long> getIdCentroInstalacion(Long idSupervisor) {
 		Criteria c = getCurrentSession().createCriteria(GerenteSupervisorDTO.class);
 		c.createAlias("centroInstalacion", "CI");
-		c.add(Restrictions.eq("gerente", idSupervisor));
-		c.add(Restrictions.eq("supervisor", idSupervisor));
+		Criterion gerente = Restrictions.eq("supervisor", idSupervisor);
+        Criterion supervisor = Restrictions.eq("name", idSupervisor);
+        LogicalExpression orExp = Restrictions.or(gerente, supervisor);
+        c.add(orExp);
 		c.add(Restrictions.eq("stActivo", Boolean.TRUE));
+		c.add(Restrictions.eq("CI.stActivo", Boolean.TRUE));
 		c.setProjection(Projections.distinct(Projections.property("CI.idCentroInstalacion")));
 		return (List<Long>)c.list();
 
