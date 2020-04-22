4b744162a75e75e6dbdb7306a55a236631b9dfda
Daniel <daniel.wolf2.gc@gmail.com>
2020-04-22 09:59:53 -0500

    Adecuacion para consulta de imagenes a nivel encuesta

--- siye-api/src/main/java/mx/com/teclo/siye/persistencia/hibernate/dao/encuesta/UsuarioEncuestaDAOImpl.java
+++ siye-api/src/main/java/mx/com/teclo/siye/persistencia/hibernate/dao/encuesta/UsuarioEncuestaDAOImpl.java
@@ -68,4 +68,19 @@ public class UsuarioEncuestaDAOImpl extends BaseDaoHibernate<OrdenEncuestaDTO> i
 		c.add(Restrictions.eq("stActivo", true));
 		return (List<OrdenEncuestaDTO>)c.list();
 	}
+
+	@Override
+	public OrdenEncuestaDTO consultaByIdOrdernIdEncuesta(Long idOrden, Long idEnceusta) {
+		Criteria c= getCurrentSession().createCriteria(OrdenEncuestaDTO.class);
+		c.createAlias("ordenServicio", "ordenServicio");
+		c.createAlias("encuesta", "encuesta");
+		
+		c.add(Restrictions.eq("ordenServicio.idOrdenServicio", idOrden));
+		c.add(Restrictions.eq("ordenServicio.stActivo", true));
+		c.add(Restrictions.eq("encuesta.idEncuesta", idEnceusta));
+		c.add(Restrictions.eq("encuesta.stActivo", 1));
+		c.add(Restrictions.eq("stActivo", true));
+		
+		return (OrdenEncuestaDTO)c.uniqueResult();
+	}
 }
