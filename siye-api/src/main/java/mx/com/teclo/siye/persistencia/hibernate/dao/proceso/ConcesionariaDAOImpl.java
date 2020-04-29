bfca941e241e3a77d4ed576066f8cd49aa1b2e16
unknown <estephanie.chavez@unitis.com.mx>
2020-04-28 19:41:52 -0500

    actualizacion catalogos

--- siye-api/src/main/java/mx/com/teclo/siye/persistencia/hibernate/dao/proceso/ConcesionariaDAOImpl.java
+++ siye-api/src/main/java/mx/com/teclo/siye/persistencia/hibernate/dao/proceso/ConcesionariaDAOImpl.java
@@ -8,7 +8,9 @@ import org.hibernate.criterion.Restrictions;
 import org.springframework.stereotype.Repository;
 
 import mx.com.teclo.arquitectura.persistencia.comun.dao.BaseDaoHibernate;
+import mx.com.teclo.siye.persistencia.hibernate.dto.catalogo.ProveedorDTO;
 import mx.com.teclo.siye.persistencia.hibernate.dto.proceso.ConsecionarioDTO;
+import mx.com.teclo.siye.persistencia.hibernate.dto.proceso.VehiculoDTO;
 
 @Repository
 public class ConcesionariaDAOImpl extends BaseDaoHibernate <ConsecionarioDTO> implements ConcesionariaDAO{
@@ -19,6 +21,16 @@ public class ConcesionariaDAOImpl extends BaseDaoHibernate <ConsecionarioDTO> im
 		Criteria c = getCurrentSession().createCriteria(ConsecionarioDTO.class);
 		c.add(Restrictions.eq("stActivo", true));
 		c.addOrder(Order.asc("nuOrden"));
+		return (List<ConsecionarioDTO>) c.list();
+	}
+
+	
+	@SuppressWarnings("unchecked")
+	@Override
+	public List<ConsecionarioDTO> obtenerConsecionarioVisible(Long value) {
+		Criteria c = getCurrentSession().createCriteria(ConsecionarioDTO.class);
+		c.add(Restrictions.eq("stActivo", true));
+		c.add(Restrictions.eq("stConcesion", value));
 		return (List<ConsecionarioDTO>) c.list();
 	}
 
