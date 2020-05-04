aaab939f040266c0695acd81d438cec88fd6f151
davidGuerra2794 <dguerradelamora@gmail.com>
2020-05-04 18:32:09 -0500

    VALIDACIONES CONSULTA SIMPLE Y AVANZADA

--- siye-app/src/main/webapp/static/js/controllers/ordenServicio/consultaGeneralController.js
+++ siye-app/src/main/webapp/static/js/controllers/ordenServicio/consultaGeneralController.js
@@ -6,6 +6,19 @@ function($scope, showAlert, $location, growl, consultaGeneralService,encuestaSer
 	fhInicio:null,fhFin:null,centroInstalacion:null,
 	estatusSeguimiento:null,isLote:false,isIncidencia:false,
 	valorLoteIncidencia:null,tipoKit:null,tipoPlan:null};
+	$scope.listOrden=[];
+	
+    $scope.tabla = new Object({
+        order: '',
+        reverse: false,
+        paginacion: [5, 10, 20, 50],
+        cantidadPaginacion: 5,
+        cantidadPaginacionVal: 5,
+        view: new Object({
+            filterSearchVal: '',
+            rowsPerPageVal: 0
+        })
+    });
 	
 	
 	$scope.catalogoParamBusqueda = 
@@ -92,17 +105,34 @@ function($scope, showAlert, $location, growl, consultaGeneralService,encuestaSer
 		if($scope.consultaOrden.$invalid){
 			requiredFields()
 		}else{
+			$scope.listOrden = [];
+			$scope.mostrarTabla=false;
 			validarParametros();	
 			consultaGeneralService.busquedaTramitesParametros($scope.parametrosBusqueda).success(function(data) {
 						if(data.length >= $scope.cantidadRegistrosMostrar){
 							growl.info("Su consulta supera los "+$scope.cantidadRegistrosMostrar+" registros. En caso de requerir el reporte completo, solicítelo a soporte.", 'templateModalAviso');
 						}
-						$scope.listOrden = data;
 						consultaGeneralService.parametrosBusquedaAnterior=$scope.parametrosBusqueda;
 						$scope.mostrarTabla=true;
+						if($scope.parametrosBusqueda.tipoKit.length>0)
+						{ 
+                           var nuevoArreglo=[];
+                           var tamaño=0;
+                           nuevoArreglo.push(data[0]);
+				            for (let x = 1; x < data.length; x++) {
+				            	tamaño=nuevoArreglo.length;
+				                if (nuevoArreglo[tamaño-1].cdOrdenServicio!=data[x].cdOrdenServicio) {
+				                nuevoArreglo.push(data[x])
+				                }
+				            }
+			                $scope.listOrden = nuevoArreglo;
+						}
+						else
+						$scope.listOrden = data;
+
 						
 				}).error(function(data){
-					$scope.listOrden = {};
+					$scope.listOrden = [];
 					growl.warning(data.message);
 					$scope.mostrarTabla=false;
 				});
@@ -171,6 +201,8 @@ function($scope, showAlert, $location, growl, consultaGeneralService,encuestaSer
 	};
 	
 	$scope.cargarTramitesDia = function(){	
+		    $scope.listOrden = [];
+		    $scope.mostrarTabla=false;
 			consultaGeneralService.cargarTramitesDia().success(function(data) {
 						if(data.length >= $scope.cantidadRegistrosMostrar){
 							growl.info("Su consulta supera los "+$scope.cantidadRegistrosMostrar+" registros. En caso de requerir el reporte completo, solicítelo a soporte.", 'templateModalAviso');
@@ -179,7 +211,7 @@ function($scope, showAlert, $location, growl, consultaGeneralService,encuestaSer
 						$scope.mostrarTabla=true;
 						
 				}).error(function(data){
-					$scope.listOrden = {};
+					$scope.listOrden = [];
 					growl.warning(data.message);
 					$scope.mostrarTabla=false;
 				});
@@ -262,6 +294,7 @@ function($scope, showAlert, $location, growl, consultaGeneralService,encuestaSer
 	
 	$scope.changeOrigen=function()
 	{
+		$scope.valorOrigen="";
 		if($scope.isLote)
 			$scope.isIncidencia=false;
 
@@ -270,9 +303,81 @@ function($scope, showAlert, $location, growl, consultaGeneralService,encuestaSer
 	
 	$scope.changeOrigen2=function()
 	{
+		$scope.valorOrigen="";
 		if($scope.isIncidencia)
 			$scope.isLote=false;
 	}
+	
+    //  metodo para generarReporteExcel
+    $scope.descargarExcel = function() {
+        let peticionReporteVO = new Object();
+        peticionReporteVO.header = getCabeceras();
+        peticionReporteVO.values = getContenido($scope.listOrden);
+        peticionReporteVO.titulo = "Reporte Orden de Servicio";
+        consultaGeneralService.descargarReporteExcel(peticionReporteVO).success(function(data, status, headers) {
+            let filename = headers('filename');
+            let file = new Blob([data], { type: 'application/vnd.ms-excel;base64,' });
+            consultaGeneralService.downloadfile(file, filename);
+        }).error(function(data) {
+            mostrarAviso(data);
+        });
+    };
+    
+    getCabeceras = () => {
+        let headers = new Array();
+        headers.push("ESTATUS");
+        headers.push("ORDEN DE SERVICIO");
+        headers.push("FECHA DE CITA");
+        headers.push("PLACA");
+        headers.push("TIPO DE VEHÍCULO");
+        headers.push("TIPO DE KIT");
+        headers.push("CENTRO DE INSTALACIÓN");
+        headers.push("TIPO DE PLAN");
+        headers.push("ORIGEN");
+        headers.push("PROCESO ACTUAL");
+        return headers;
+    };
+    
+    getContenido = (list) => {
+        let array = new Array();
+        for (let i = 0; i < list.length; i++) {
+            let elemento = new Array();
+            elemento.push(list[i].stSeguimiento.nbStSeguimiento);
+            elemento.push(list[i].cdOrdenServicio);
+            elemento.push(list[i].fhCita);
+            elemento.push(list[i].vehiculo.cdPlacaVehiculo);
+            elemento.push(list[i].vehiculo.tipoVehiculo.nbTipoVehiculo);
+            elemento.push(list[i].kitInstalacion!=null && 
+            		
+            		list[i].kitInstalacion.kitInstalacionDispDTO.length>=1?list[i].kitInstalacion.kitInstalacionDispDTO[0].kitDispositivo.tipoKit.nbTipoKit:"SIN KIT ASIGNADO");
+            elemento.push(list[i].centroInstalacion.nbCentroInstalacion);
+            elemento.push(list[i].plan.nbPlan);
+            if(list[i].idOrigenOds==1 || list[i].idOrigenOds==null)
+            elemento.push(list[i].loteOrdenServicio!=null?"Lote: "+list[i].loteOrdenServicio.nbArchivoFinal:"Lote: ");	
+            if(list[i].idOrigenOds==2)
+            elemento.push("Incidencia: "+list[i].cdOrdenServicio);	
+            elemento.push(list[i].proceso == null ? "SIN PROCESO" : list[i].proceso.nbProceso);
+            array.push(elemento);
+        }
+        return array;
+    };
+    
+    mostrarAviso = (data) => {
+        if (data != undefined && data.status === 404) {
+        	growl.error(data.message, { ttl: 5000 });
+        } else if (data != undefined && data.status === 400) {
+        	growl.error(data.message, { ttl: 5000 });
+        }
+         else {
+            growl.error('Ocurrió un error en la operación', { ttl: 5000 });
+        }
+    };
+    
+	$scope.redirigir = function(id_orden){
+		$location.path('/detalleConsulta/'+id_orden);
+			
+	
+	};
 
 
 
