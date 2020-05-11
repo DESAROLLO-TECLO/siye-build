69c84c384d9f219484d49fc061c26a3f42628163
Fernando <fernando.campero.213@gmail.com>
2020-05-11 14:08:52 -0500

    Se modifico la consulta del detalle de la orden de servicio con observaciones tras junta de seguimiento

--- siye-app/src/main/webapp/static/js/controllers/ordenServicio/detalleConsultaController.js
+++ siye-app/src/main/webapp/static/js/controllers/ordenServicio/detalleConsultaController.js
@@ -1,36 +1,29 @@
 angular.module(appTeclo).controller('detalleConsultaController', 
-function($scope, showAlert, $location, growl, ordenInfo, consultaServicioService) {
+function($scope, showAlert, $location, growl, ordenInfo, consultaServicioService, expedienteService) {
 	$scope.ordenInfo = ordenInfo.data;
+	$scope.evidenciaMostrar = {};
+	$scope.supervisores=[];
 
-	calcularDuracion = function(){
-//		$scope.ordenInfo.fhAtencionIni = "2020-05-05 07:30:00";
-		if($scope.ordenInfo.fhAtencionIni!=null){
-	//		var fecha1 = moment("2016-09-30 07:30:00", "YYYY-MM-DD HH:mm");
-	//		var fecha2 = moment("2016-10-03 08:31:26", "YYYY-MM-DD HH:mm");
+	calcularDuracion = function(fhIni, fhFin){
+		if(fhIni!=null){
 			
-			var fecha1 = moment($scope.ordenInfo.fhAtencionIni, "YYYY-MM-DD HH:mm");
+			var fecha1 = moment(fhIni, "DD/MM/YYYY HH:mm:ss");
 			var fecha2 = null
-			if($scope.ordenInfo.fhAtencionFin!=null){
-				fecha2 = moment($scope.ordenInfo.fhAtencionFin != null , "YYYY-MM-DD HH:mm");
-			}else{
+			if(fhFin!=null)
+				fecha2 = moment(fhFin, "DD/MM/YYYY HH:mm:ss");
+			else
 				fecha2 = moment();
-			}
-			
-			$scope.ordenInfo
-			
-			var now = moment();
 			
 			var diff = fecha2.diff(fecha1, 's'); // Diff in hours
-	//		console.log(diff);
 			
 			var horas = Math.trunc(diff / 3600);
 		    var minutos = Math.trunc((diff-horas*3600)/60);
 		    var segundos = diff-(horas*3600+minutos*60);
-		    console.log(horas + ":" + minutos + ":" + segundos);
+		    var diferencia =  (horas<10 ? ("0"+horas): horas) + ":" + (minutos<10?("0"+minutos):minutos) + ":" + (segundos<10?("0"+segundos):segundos);
 			
-		    $scope.ordenInfo.tpDuracion = horas + ":" + minutos + ":" + segundos;
+		    return diferencia;
 		}else{
-			$scope.ordenInfo.tpDuracion = "00:00:00";
+			return "00:00:00";
 		}
 	}
 	
@@ -41,7 +34,9 @@ function($scope, showAlert, $location, growl, ordenInfo, consultaServicioService
 			$scope.ordenInfo.cdOrigenOds = "INCIDENCIA"
 		}
 		$scope.ordenInfo.centroInstalacion.nbCentroInstalacion 
-			= $scope.ordenInfo.centroInstalacion.nbCentroInstalacion.toUpperCase();  
+			= $scope.ordenInfo.centroInstalacion.nbCentroInstalacion.toUpperCase();
+		
+		$scope.ordenInfo.tpDuracion = calcularDuracion($scope.ordenInfo.fhAtencionIni, $scope.ordenInfo.fhAtencionFin);
 	}
 	
 	consultarTransportistas = function(){
@@ -56,7 +51,7 @@ function($scope, showAlert, $location, growl, ordenInfo, consultaServicioService
 	            $scope.sintransportista = "";
 	        }).error(function(error){
 	            console.log(error);
-	            $scope.transportista=[];
+	            $scope.transportistas = [];
 	            $scope.transportista="";
 	            $scope.sintransportista = "Sin Transportistas";
 	        });
@@ -69,12 +64,89 @@ function($scope, showAlert, $location, growl, ordenInfo, consultaServicioService
 		$scope.sinSupervisor = "Sin Supervisores";
 	   
 	};
+	
+	consultaEvidencia = function(){
+//		expedienteService.getInfoOs("ORDEN_SERVICIO", $scope.ordenInfo.cdOrdenServicio).success(function(data){
+		expedienteService.getInfoDetalleOSNivel("ORDEN_SERVICIO", $scope.ordenInfo.cdOrdenServicio).success(function(data){
+			$scope.evidencia = data;
+			$scope.error = false;
+			
+			$scope.evidencia.infoEvidencia.activo = false;
+			$scope.supervisor = $scope.evidencia.infoEvidencia.nbSupervisor != null ? $scope.evidencia.infoEvidencia.nbSupervisor[0] : "Sin Supervisor";
+			for(sup in $scope.evidencia.infoEvidencia.nbSupervisor){
+				var sup = {"name":$scope.evidencia.infoEvidencia.nbSupervisor[sup]};
+				$scope.supervisores.push(sup);
+			}
+			
+			for(proceso in $scope.evidencia.procesos){
+				$scope.evidencia.procesos[proceso].infoEvidencia.activo = false;
+				for(encuesta in $scope.evidencia.procesos[proceso].listEncuestas){
+					$scope.evidencia.procesos[proceso].listEncuestas[encuesta].infoEvidencia.activo = false;
+					for(pregunta in $scope.evidencia.procesos[proceso].listEncuestas[encuesta].listPreguntas){
+						$scope.evidencia.procesos[proceso].listEncuestas[encuesta].listPreguntas[pregunta].infoEvidencia.activo = false;
+						$scope.evidencia.procesos[proceso].listEncuestas[encuesta].listPreguntas[pregunta].infoEvidencia.tieneImagen = 
+							$scope.evidencia.procesos[proceso].listEncuestas[encuesta].infoEvidencia.activo
+						$scope.evidencia.procesos[proceso].listEncuestas[encuesta].listPreguntas[pregunta].infoEvidencia.fechaIni = 
+							$scope.evidencia.procesos[proceso].listEncuestas[encuesta].infoEvidencia.fechaIni;
+						$scope.evidencia.procesos[proceso].listEncuestas[encuesta].listPreguntas[pregunta].infoEvidencia.fechaFin = 
+							$scope.evidencia.procesos[proceso].listEncuestas[encuesta].infoEvidencia.fechaFin;
+						$scope.evidencia.procesos[proceso].listEncuestas[encuesta].listPreguntas[pregunta].infoEvidencia.nbSupervisor = 
+							$scope.evidencia.procesos[proceso].listEncuestas[encuesta].infoEvidencia.nbSupervisor;
+						$scope.evidencia.procesos[proceso].listEncuestas[encuesta].listPreguntas[pregunta].infoEvidencia.nbInstalador = 
+							$scope.evidencia.procesos[proceso].listEncuestas[encuesta].infoEvidencia.nbInstalador;
+						$scope.evidencia.procesos[proceso].listEncuestas[encuesta].listPreguntas[pregunta].infoEvidencia.nbTrasportista = 
+							$scope.evidencia.procesos[proceso].listEncuestas[encuesta].infoEvidencia.nbTrasportista;
+						$scope.evidencia.procesos[proceso].listEncuestas[encuesta].listPreguntas[pregunta].infoEvidencia.imagenes = 
+							$scope.evidencia.procesos[proceso].listEncuestas[encuesta].infoEvidencia.imagenes;
+					}
+				}
+			}
+			
+			$scope.mostrarInfoEvidencia("Orden de Servicio", $scope.evidencia);
+//	$scope.mostrarInfoEvidencia("Orden de Servicio", null, null, 
+//			"Sin supervisor", "Sin instalador", "Sin Trasportista", $scope.listaImagenes, $scope.evidencia);
+			
+		}).error(function(data){
+			$scope.evidencia = {};
+			$scope.error = data;
+		});
+	}
+	
+	$scope.mostrarInfoEvidencia = function(nivelNombre, obj){
+		$scope.evidenciaMostrar.nombre = nivelNombre;
+		$scope.evidenciaMostrar.fhIni = obj.infoEvidencia.fechaIni;
+		$scope.evidenciaMostrar.fhFin = obj.infoEvidencia.fechaFin;
+		$scope.evidenciaMostrar.duracion = calcularDuracion(obj.infoEvidencia.fechaIni, obj.infoEvidencia.fechaFin); 
+		$scope.evidenciaMostrar.supervisor = obj.infoEvidencia.nbSupervisor != null ? obj.infoEvidencia.nbSupervisor[0] : "Sin Supervisor";
+		$scope.evidenciaMostrar.instalador = obj.infoEvidencia.nbInstalador != null ? obj.infoEvidencia.nbInstalador[0] : "Sin Instalador";
+		$scope.evidenciaMostrar.transportista = obj.infoEvidencia.nbTrasportista != null ? obj.infoEvidencia.nbTrasportista[0] : "Sin Trasportista";
+		
+		$scope.evidenciaMostrar.supervisores = obj.infoEvidencia.nbSupervisor;
+		$scope.evidenciaMostrar.instaladores = obj.infoEvidencia.nbInstalador;
+		$scope.evidenciaMostrar.transportistas = obj.infoEvidencia.nbTrasportista;
+		
+		
+		$scope.evidenciaMostrar.listImg = obj.infoEvidencia.imagenes;
+		
+		$scope.evidencia.infoEvidencia.activo = false;
+		for(proceso in $scope.evidencia.procesos){
+			$scope.evidencia.procesos[proceso].infoEvidencia.activo = false;
+			for(encuesta in $scope.evidencia.procesos[proceso].listEncuestas){
+				$scope.evidencia.procesos[proceso].listEncuestas[encuesta].infoEvidencia.activo = false;
+				for(pregunta in $scope.evidencia.procesos[proceso].listEncuestas[encuesta].listPreguntas){
+					$scope.evidencia.procesos[proceso].listEncuestas[encuesta].listPreguntas[pregunta].infoEvidencia.activo = false;
+				}
+			}
+		}
+		
+		obj.infoEvidencia.activo = true;
+	}
 
 	consultarTransportistas();
 	consultarSupervisores();
 	formateaDatos();
-	calcularDuracion();
-	
+//	calcularDuracion();
+	consultaEvidencia();
 });
 
 //
