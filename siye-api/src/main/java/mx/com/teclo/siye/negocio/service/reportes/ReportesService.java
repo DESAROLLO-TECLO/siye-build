75bf9386c2a064bf01107ba4bf00bffe45974c79
Fernando <fernando.campero.213@gmail.com>
2020-05-19 19:49:41 -0500

    Se modifico el servicio que genera los reportes del detalle de orden de servicio, se agrego sub reporte

--- siye-api/src/main/java/mx/com/teclo/siye/negocio/service/reportes/ReportesService.java
+++ siye-api/src/main/java/mx/com/teclo/siye/negocio/service/reportes/ReportesService.java
@@ -7,7 +7,7 @@ import mx.com.teclo.siye.persistencia.vo.proceso.OrdenServicioVO;
 
 public interface ReportesService {
 	
-	ByteArrayOutputStream getReporteDOS(OrdenServicioVO os, CargaExpedienteImgVO detalle);
+	ByteArrayOutputStream getReporteDOS(OrdenServicioVO os, CargaExpedienteImgVO detalle, Boolean conImagenes);
 
 	byte[] getPDF(ByteArrayOutputStream b);
 }
