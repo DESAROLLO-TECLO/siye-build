21a41b7ea0e8efa351b4bea4048731fe63b990ca
teclofjmb <francisco.martinez@teclo.mx>
2020-07-14 13:14:05 -0500

    Cambios para resolver conflicto con contexto

--- siye-app/src/main/java/mx/com/teclo/WebAppConfig.java
+++ siye-app/src/main/java/mx/com/teclo/WebAppConfig.java
@@ -1,8 +1,8 @@
 package mx.com.teclo;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
-import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @EnableWebMvc
